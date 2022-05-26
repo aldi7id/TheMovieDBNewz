a522f00351587ba3fc03b92fef1266ca8e5fcb5c
aldi7id <aldi7.indo@gmail.com>
2022-05-26 22:44:01 +0700

    Add Flavoring

--- app/src/main/java/com/ajgroup/themoviedbnew/MainActivity.kt
+++ app/src/main/java/com/ajgroup/themoviedbnew/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ajgroup.themoviedbnew
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ajgroup.themoviedbnew.R
+import org.koin.android.BuildConfig
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
