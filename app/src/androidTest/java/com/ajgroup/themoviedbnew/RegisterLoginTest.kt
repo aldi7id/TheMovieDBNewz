996afb569a9ba7a7748959775ed4738bc63e847a
aldi7id <aldi7.indo@gmail.com>
2022-05-26 22:15:32 +0700

    Unit Test & UI Test

---
+++ app/src/androidTest/java/com/ajgroup/themoviedbnew/RegisterLoginTest.kt
@@ -0,0 +1,259 @@
+package com.ajgroup.themoviedbnew
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class RegisterLoginTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun registerLoginTest() {
+        Thread.sleep(5000)
+        val materialTextView = onView(
+            allOf(
+                withId(R.id.SignUpButton), withText("Sign Up"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.myNavHostFragment),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialTextView.perform(click())
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.et_email),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_email),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("aldi1@gmail.com"), closeSoftKeyboard())
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.et_email), withText("aldi1@gmail.com"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_email),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(pressImeActionButton())
+
+        val textInputEditText3 = onView(
+            allOf(
+                withId(R.id.et_name),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_name),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText3.perform(replaceText("aldi"), closeSoftKeyboard())
+
+        val textInputEditText4 = onView(
+            allOf(
+                withId(R.id.et_name), withText("aldi"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_name),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText4.perform(pressImeActionButton())
+
+        val textInputEditText5 = onView(
+            allOf(
+                withId(R.id.et_password),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_password),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText5.perform(replaceText("1"), closeSoftKeyboard())
+
+        val textInputEditText6 = onView(
+            allOf(
+                withId(R.id.et_password), withText("1"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_password),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText6.perform(pressImeActionButton())
+
+        val textInputEditText7 = onView(
+            allOf(
+                withId(R.id.et_password2),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_password2),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText7.perform(replaceText("1"), closeSoftKeyboard())
+
+        val textInputEditText8 = onView(
+            allOf(
+                withId(R.id.et_password2), withText("1"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_password2),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText8.perform(pressImeActionButton())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_register), withText("Register"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.myNavHostFragment),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textInputEditText9 = onView(
+            allOf(
+                withId(R.id.et_email),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_email),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText9.perform(replaceText("aldi1@gmail.com"), closeSoftKeyboard())
+
+        val textInputEditText10 = onView(
+            allOf(
+                withId(R.id.et_email), withText("aldi1@gmail.com"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_email),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText10.perform(pressImeActionButton())
+
+        val textInputEditText11 = onView(
+            allOf(
+                withId(R.id.et_password),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.til_password),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText11.perform(replaceText("1"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btn_login), withText("Login"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.myNavHostFragment),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/ajgroup/themoviedbnew/repository/VerifRepositoryTest.kt
@@ -0,0 +1,47 @@
+package com.ajgroup.themoviedbnew.repository
+
+import com.ajgroup.themoviedbnew.data.local.UserDao
+import com.ajgroup.themoviedbnew.data.local.UserDataStoreManager
+import com.ajgroup.themoviedbnew.data.local.model.User
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class VerifRepositoryTest {
+    private lateinit var userDao: UserDao
+    private lateinit var userDataStoreManager: UserDataStoreManager
+
+    private lateinit var verifRepository: VerifRepository
+    @Before
+    fun setUp() {
+        userDao = mockk()
+        userDataStoreManager = mockk()
+        verifRepository = VerifRepository(userDao, userDataStoreManager)
+    }
+
+    @Test
+    fun login() {
+        val returnLogin = mockk<User>()
+
+        every {
+            runBlocking {
+                userDao.login("email", "password")
+            }
+        } returns returnLogin
+
+        verifRepository.login("email", "password")
+
+        verify {
+            runBlocking {
+                userDao.login("email", "password")
+            }
+        }
+
+
+    }
+}
